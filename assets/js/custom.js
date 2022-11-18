ee9f0424d55fe62299cfcb1de8cd3dab3de552fc
Jeremy Alexander <jeremy.alexander@ubc.ca>
2022-11-18 12:45:41 -0800

    'changed import cmd in js file for bootstrap js'

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,4 @@
-import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../../node_modules/bootstrap/dist/js/bootstrap.min.js";
 
 gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
 
